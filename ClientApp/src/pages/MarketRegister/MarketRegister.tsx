54439a27999587246fba44d15375433575914b30
Tom <ec.tom.n@gmail.com>
2020-03-22 09:20:29 +0100

    added register validation

--- ClientApp/src/pages/MarketRegister/MarketRegister.tsx
+++ ClientApp/src/pages/MarketRegister/MarketRegister.tsx
@@ -14,47 +14,64 @@ import Api from "../../Utils/FakeApi";
 import "./MarketRegister.styles.css";
 
 export const MarketRegister = () => {
+  let [marktId, setMarktId] = React.useState("");
   let [username, setUsername] = React.useState("");
   let [password, setPassword] = React.useState("");
   let [passwordValidation, setPasswordValidation] = React.useState("");
+  let [errorMessage, setErrorMessage] = React.useState("");
   const currentUser = useSelector(
     (state: RootStateType) => state.user.currentUser
   );
   const dispatch = useDispatch();
   const populateRegisterData = async function() {
+    if (password === "" || username === "" || marktId === "") {
+      setErrorMessage("Bitte füllen sie alle Felder aus");
+      return;
+    }
+    if (password != passwordValidation) {
+      setErrorMessage("Die Passwörter stimmen nicht überein");
+      return;
+    }
+    if (password.length < 8) {
+      setErrorMessage("Ihr Passwort ist zu kurz (mind. 8 Zeichen)");
+      return;
+    }
     Api.Login(username, password).then(data => {
       console.log("TODO: go to auth");
     });
   };
 
-  if (currentUser != null) {
-    console.log("login successful:", "redirect to /updatestatus");
-    return <Redirect to={{ pathname: "/updatestatus" }} />;
-  }
-
   return (
-    <div className="login_page">
+    <div className='register_page'>
       <InputField
-        placeholder="Username"
+        placeholder='Markt Id'
+        Type={InputFieldTypes.Text}
+        onChange={value => {
+          setMarktId(value);
+        }}
+      />
+      <InputField
+        placeholder='Username'
         Type={InputFieldTypes.Text}
         onChange={value => {
           setUsername(value);
         }}
       />
       <InputField
-        placeholder="Password"
+        placeholder='Passwort'
         Type={InputFieldTypes.Password}
         onChange={value => {
           setPassword(value);
         }}
       />
       <InputField
-        placeholder="Password"
+        placeholder='Passwort wiederholen'
         Type={InputFieldTypes.Password}
         onChange={value => {
           setPasswordValidation(value);
         }}
       />
+      <div className='error_message'>{errorMessage}</div>
       <Button
         Type={ButtonTypes.Confirm}
         onClick={() => {
